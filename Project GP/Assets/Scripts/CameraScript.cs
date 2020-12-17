1207c62056f530caf0813ef78ce94c7b9a0309fe
Gabriel Chan <gaibryol@gmail.com>
2020-12-17 11:24:18 -0700

    Made small changes to camera script

--- Project GP/Assets/Scripts/CameraScript.cs
+++ Project GP/Assets/Scripts/CameraScript.cs
@@ -7,16 +7,18 @@ public class CameraScript : MonoBehaviour
     // Get player
     public GameObject player;
 
-    // Z-axis distance from camera to player
-    float distance = -30f;
-
     // Used to create a Vector3 position, since you can't individually change the x, y, and z values of an objects position
     Vector3 pos;
+
+    // Used to handle camera size
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // Find camera component
+        cam = gameObject.GetComponent<Camera>();
+        cam.orthographicSize = 6.0f;
     }
 
     // Update is called once per frame
@@ -25,11 +27,7 @@ public class CameraScript : MonoBehaviour
         // Set pos to player's position
         pos = player.transform.position;
 
-        // Update distance
-        pos.z += distance;
-
         // Change camera position to pos
-        transform.position = pos + new Vector3(0, 3, 0);
-        
+        transform.position = pos + new Vector3(0, 3, -1);
     }
 }
