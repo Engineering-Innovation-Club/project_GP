77f29282431e3aae5a0135d21187d563ee68ad11
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2021-01-13 01:21:40 -0700

    This script allows the bot to shoot projectiles of bullets out of it
    Player Prefab edit: Added a firepoint as a child of the bot to indicate where the bullets will be coming from.
    ProjectileShootingScript added: Will take the bullet as a game object, and will instantiate the bullet and shoot towards the direction of the mouse.
    
    I have not added the damage of every shot and how it is going to affect enemies. I still need to read enemy script and understand so I can implement properly.

---
+++ Project GP/Assets/Scripts/ProjectileShootingScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShootingScript : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask whatToHit;
+    //public string firePointOfWeapon; 
+    //For every weapon the barrel of the gun is the fire point, simce that is where bullets come out. 
+
+    float timeToFire = 0;
+    Transform firePoint;
+    [SerializeField] private Transform Bullet;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //FirePoint will usually be the child of the gun or weapon. 
+        //firePoint = transform.Find(firePointOfWeapon);
+
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("Warning!\n No Fire Point!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Single Fire-Rate
+        if (fireRate == 0)
+        {
+            if (Input.GetKeyDown("mouse 0"))
+            {
+                shoot();
+            }
+        }
+
+        else
+        {
+            if (Input.GetKeyDown("mouse 0") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                shoot();
+            }
+        }
+    }
+
+    void shoot()
+    {
+        //Debug.LogError("Shots fired");
+        // Transfer the mouse position from the screen coordinate from the display position to the game world
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+
+        Instantiate(Bullet, firePointPosition, Quaternion.identity);
+    }
+}
