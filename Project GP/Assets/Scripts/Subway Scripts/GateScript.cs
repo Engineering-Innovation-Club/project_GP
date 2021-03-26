806b445c2a2c956462687e1bd3d90a31a722cf19
Gabriel Chan <gaibryol@gmail.com>
2021-03-25 23:07:19 -0600

    Added gate

---
+++ Project GP/Assets/Scripts/Subway Scripts/GateScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    public bool isOpen;
+    BoxCollider2D coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovementControl pScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementControl>();
+        
+        if (pScript.hidBot)
+        {
+            isOpen = true;
+        }
+        else
+        {
+            isOpen = false;
+        }
+
+        if (isOpen)
+        {
+            coll.enabled = false;
+        }
+        else
+        {
+            coll.enabled = true;
+        }
+    }
+}
