2652edfb0fc1aff29b9cc189cdf4112ab0e664ac
Gabriel Chan <gaibryol@gmail.com>
2021-03-30 16:10:17 -0600

    Added save checkpoints

---
+++ Project GP/Assets/Scripts/Save Scripts/SaveCheckpointScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCheckpointScript : MonoBehaviour
+{
+    private PlayerManager pSaveScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pSaveScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pSaveScript.SaveStats();
+        }
+    }
+}
