92669069f222621bb69b9f60c65f7556cb10be09
Gabriel Chan <gaibryol@gmail.com>
2021-03-30 19:08:23 -0600

    Added save message

---
+++ Project GP/Assets/Scripts/Miscellaneous/SaveTextScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveTextScript : MonoBehaviour
+{
+    private Text text;
+    public float timeAppear;
+    private bool fading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        timeAppear = 5f;
+        text.canvasRenderer.SetAlpha(0);
+        text.CrossFadeAlpha(0, 0f, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fading)
+        {
+            timeAppear -= Time.deltaTime;
+        }
+
+        if (timeAppear <= 0)
+        {
+            text.CrossFadeAlpha(0, 0.5f, false);
+            fading = false;
+        }
+        
+    }
+
+    public void FadeAnimation()
+    {
+        Debug.Log("fading");
+        timeAppear = 5f;
+        fading = true;
+
+        text.canvasRenderer.SetAlpha(0.01f);
+        text.CrossFadeAlpha(1, 3f, false);
+    }
+}
--- Project GP/Assets/Scripts/Save Scripts/SaveCheckpointScript.cs
+++ Project GP/Assets/Scripts/Save Scripts/SaveCheckpointScript.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveCheckpointScript : MonoBehaviour
 {
     private PlayerManager pSaveScript;
+    private SaveTextScript textScript;
+
+    public GameObject saveText;
+    public string message;
+
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
         pSaveScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+        textScript = saveText.GetComponent<SaveTextScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            pSaveScript.SaveStats();
+            if (timer <= 0f)
+            {
+                pSaveScript.SaveStats();
+                textScript.FadeAnimation();
+
+                saveText.GetComponent<Text>().text = message;
+
+                timer = 5f;
+            }
+            
         }
     }
 }
