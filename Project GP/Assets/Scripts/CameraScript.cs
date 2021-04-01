8a80fae28bcd6ad5fd3531d595f060d20fb13cd2
Andrew Chen <akchen@ualberta.ca>
2021-03-31 19:12:29 -0600

    campus

--- Project GP/Assets/Scripts/CameraScript.cs
+++ Project GP/Assets/Scripts/CameraScript.cs
@@ -6,19 +6,28 @@ public class CameraScript : MonoBehaviour
 {
     // Get player
     public GameObject player;
+    public bool isBounded;
+    public Transform leftBound;
+    public Transform rightBound;
+    public Transform topBound;
+    public Transform botBound;
 
     // Used to create a Vector3 position, since you can't individually change the x, y, and z values of an objects position
     Vector3 pos;
 
     // Used to handle camera size
     private Camera cam;
+    private float xSize;
+    private float ySize;
 
     // Start is called before the first frame update
     void Start()
     {
         // Find camera component
         cam = gameObject.GetComponent<Camera>();
-        cam.orthographicSize = 6.0f;
+        cam.orthographicSize = 10.0f;
+        xSize = Camera.main.orthographicSize * Screen.width / Screen.height;
+        ySize = Camera.main.orthographicSize;       
     }
 
     // Update is called once per frame
@@ -28,6 +37,16 @@ public class CameraScript : MonoBehaviour
         pos = player.transform.position;
 
         // Change camera position to pos
-        transform.position = pos + new Vector3(0, 3, -1);
+        if (isBounded)
+        {
+            pos = pos + new Vector3(0, 3, -1);
+            pos.x = Mathf.Clamp(pos.x, leftBound.position.x + xSize, rightBound.position.x - xSize);
+            pos.y = Mathf.Clamp(pos.y, botBound.position.y + ySize, topBound.position.y - ySize);
+            transform.position = pos;
+        }
+        else
+        {
+            transform.position = pos + new Vector3(0, 3, -1);
+        }
     }
 }
---
+++ Project GP/Assets/Scripts/Miscellaneous/BackgroundLoop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private float length;
+    private float startpos;
+    public GameObject camera;
+    public float parallexEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float temp = (camera.transform.position.x * (1 - parallexEffect));
+        float dist = (camera.transform.position.x * parallexEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        } else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
