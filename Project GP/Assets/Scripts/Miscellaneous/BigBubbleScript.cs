1a4248a402aaa10ef9306b2bb2e10a50816de96c
Gabriel Chan <gaibryol@gmail.com>
2021-05-02 14:36:51 -0600

    Added chem vat animations

---
+++ Project GP/Assets/Scripts/Miscellaneous/BigBubbleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBubbleScript : MonoBehaviour
+{
+    private Animator anim;
+    private AnimatorClipInfo[] animClip;
+    private float clipTime;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        animClip = anim.GetCurrentAnimatorClipInfo(0);
+        clipTime = animClip[0].clip.length;
+
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= clipTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Project GP/Assets/Scripts/Miscellaneous/ChemVatScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemVatScript : MonoBehaviour
+{
+    public GameObject bigBubble;
+    public GameObject littleBubble;
+    public GameObject twoBubble;
+
+    private float timer;
+    public float spawnTime;
+    private float choice;
+    private Vector3 pos;
+
+    public int numSurface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = spawnTime;
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            choice = Random.Range(1, 4);
+            //Debug.Log(choice);
+            spawn((int)choice);
+
+            timer = spawnTime;
+        }
+    }
+
+    private void spawn(int type)
+    {
+
+        pos = new Vector3(Random.Range(gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.min.x + 5f, gameObject.transform.GetChild(numSurface - 1).GetComponent<SpriteRenderer>().bounds.max.x - 5f), pos.y, pos.z);
+        if (type == 1)
+        {
+            // Big Bubble
+            Instantiate(bigBubble, pos, Quaternion.identity);
+        }
+        else if (type == 2)
+        {
+            // Little Bubble
+            Instantiate(littleBubble, pos, Quaternion.identity);
+        }
+        else if (type == 3)
+        {
+            // Two Bubble
+            Instantiate(twoBubble, pos, Quaternion.identity);
+        }
+    }
+}
