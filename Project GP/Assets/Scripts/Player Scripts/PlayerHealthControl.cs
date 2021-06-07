b227d34af43a8cad732d2d1f93cd2e5be530959c
Gabriel Chan <gaibryol@gmail.com>
2021-06-07 16:42:22 -0600

    Added health bar

--- Project GP/Assets/Scripts/Player Scripts/PlayerHealthControl.cs
+++ Project GP/Assets/Scripts/Player Scripts/PlayerHealthControl.cs
@@ -8,14 +8,11 @@ public class PlayerHealthControl : MonoBehaviour
     public int health;
     public int maxHealth;
 
-    public PlayerUIScript playerUIScript;
-
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = 5;
         health = maxHealth;
-        playerUIScript.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -28,20 +25,17 @@ public class PlayerHealthControl : MonoBehaviour
     public void hit(int damage)
     {
         health -= damage;
-        //playerUIScript.SetHealth(health);
     }
 
     // heal function that when called, heals player
     public void heal(int amount)
     {
         health += amount;
-        playerUIScript.SetHealth(health);
     }
 
     // heal function that when called, heals player to full HP
     public void healToFull()
     {
         health = maxHealth;
-        playerUIScript.SetHealth(health);
     }
 }
---
+++ Project GP/Assets/Scripts/UI/HealthBarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarScript : MonoBehaviour
+{
+    public GameObject player;
+    PlayerHealthControl pHScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pHScript = player.GetComponent<PlayerHealthControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
