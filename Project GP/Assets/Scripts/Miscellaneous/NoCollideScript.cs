1136ac616bb0a831f3bc55527356021e4dd8ec5b
Gabriel Chan <gaibryol@gmail.com>
2021-06-11 20:48:48 -0600

    Fixed script so bullet can pass through

--- Project GP/Assets/Scripts/Miscellaneous/NoCollideScript.cs
+++ Project GP/Assets/Scripts/Miscellaneous/NoCollideScript.cs
@@ -6,11 +6,15 @@ using UnityEngine.Tilemaps;
 public class NoCollideScript : MonoBehaviour
 {
     public GameObject thing;
+    public GameObject bullet;
 
     // Start is called before the first frame update
     void Start()
     {
         Physics2D.IgnoreCollision(thing.GetComponent<BoxCollider2D>(), GetComponent<CompositeCollider2D>());
         Physics2D.IgnoreCollision(thing.GetComponent<BoxCollider2D>(), GetComponent<TilemapCollider2D>());
+
+        Physics2D.IgnoreCollision(bullet.GetComponent<BoxCollider2D>(), GetComponent<CompositeCollider2D>());
+        Physics2D.IgnoreCollision(bullet.GetComponent<BoxCollider2D>(), GetComponent<TilemapCollider2D>());
     }
 }
