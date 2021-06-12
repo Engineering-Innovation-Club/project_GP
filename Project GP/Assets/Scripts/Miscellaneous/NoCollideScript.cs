055847a3dfb9b1065d0c0f0c27fcc4f52ec7ed00
Gabriel Chan <gaibryol@gmail.com>
2021-06-11 20:35:46 -0600

    Added platforms that the boss cant collide with

---
+++ Project GP/Assets/Scripts/Miscellaneous/NoCollideScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class NoCollideScript : MonoBehaviour
+{
+    public GameObject thing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(thing.GetComponent<BoxCollider2D>(), GetComponent<CompositeCollider2D>());
+        Physics2D.IgnoreCollision(thing.GetComponent<BoxCollider2D>(), GetComponent<TilemapCollider2D>());
+    }
+}
