1e419ae443ce921dc826b1a0af6fab275ac06363
Gabriel Chan <gaibryol@gmail.com>
2021-08-04 17:37:41 -0600

    Fixed cutscene dialogue revisions

--- Project GP/Assets/Scripts/Miscellaneous/BossRoomTrigger.cs
+++ Project GP/Assets/Scripts/Miscellaneous/BossRoomTrigger.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
 
 public class BossRoomTrigger : MonoBehaviour
 {
@@ -9,6 +12,13 @@ public class BossRoomTrigger : MonoBehaviour
 
     public GameObject player;
     public GameObject boss;
+
+    public List<string> speakerList;
+    public List<string> messageList;
+    public List<Sprite> imageList;
+    public List<PlayableAsset> animList;
+
+    public DialogueScript dScript;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +32,14 @@ public class BossRoomTrigger : MonoBehaviour
         if (player.GetComponent<PlayerHealthControl>().health <= 0)
         {
             door.SetActive(false);
+        }
+    }
+
+    void Add(List<string> sList, List<string> mList, List<Sprite> iList, List<PlayableAsset> aList)
+    {
+        for (int i = 0; i < sList.Count; i++)
+        {
+            dScript.AddDialogue(sList[i], mList[i], iList[i], aList[i]);
         }
     }
 
